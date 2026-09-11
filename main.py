b54462666abdc9f8ba0ae4b186610929d2765e5e
星辰蝶语 <15114671359@nefu.edu.cn>
2026-09-11 22:12:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ from tkinter import messagebox, simpledialog
 
 sys.path.insert(0, os.path.dirname(__file__))
 
-APP_VERSION = 'v0.2.1'
+APP_VERSION = 'v0.3.1'
 
 # ============================================================================
 # 章节名称配置（可选）
